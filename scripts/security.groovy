5cc5695433ba15590f7f9f71e4704a9f7d3d14e4
Tobias Schifftner <ts@ambimax.de>
2019-03-15 12:00:45 +0100

    Added security enhancements

--- scripts/security.groovy
+++ scripts/security.groovy
@@ -1,6 +1,10 @@
 #!groovy
 
+import jenkins.security.s2m.AdminWhitelistRule
 import jenkins.model.Jenkins
 
 // Disable CLI over Remoting
 jenkins.model.Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
+
+// Enabling the access control mechanism
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
